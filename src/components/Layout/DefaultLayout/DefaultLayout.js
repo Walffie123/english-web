ad7dbee2d8ee236bee3743acf4f8251ead6893c7
MaZuongToiChoi <lamthdde160172@fpt.edu.vn>
2023-03-05 16:20:54 +0700

    Dung xong layout cho Web

--- src/components/Layout/DefaultLayout/DefaultLayout.js
+++ src/components/Layout/DefaultLayout/DefaultLayout.js
@@ -1,11 +1,17 @@
-import Header from './Header/Header';
+import classNames from 'classnames/bind';
+import Header from './Header/header';
+import styles from './DefaultLayout.module.scss';
+import SideBar from './SideBar/sidebar';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
-                <div className="content">{children}</div>
+            <div className={cx('container')}>
+                <SideBar />
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
--- src/pages/Upload/upload.js
+++ src/pages/Upload/upload.js
@@ -1,5 +1,3 @@
-import Header from '~/components/Layout/DefaultLayout/Header/Header';
-
 function Upload() {
     return <h2> Upload Page </h2>;
 }
