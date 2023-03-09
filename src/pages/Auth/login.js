65b2e30597f43dfacabee84d6aa98371ea821478
Thanh <hondondaide@gmail.com>
2023-03-09 16:01:11 +0700

    git merge changes from branch

--- src/pages/Auth/login.js
+++ src/pages/Auth/login.js
@@ -1,44 +1,29 @@
-import classNames from 'classnames/bind';
-import styles from './Login.module.scss';
+import React, { useState } from 'react';
+import './Login.module.scss';
 
-const cx = classNames.bind(styles);
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-function Login() {
-    return (
-        // <div>
-        //     <h1>Login</h1>
-        // </div>
-        <section>
-            <div className={cx('form-box')}>
-                <div className={cx('form-value')}>
-                    <form action="">
-                        <h2>Login</h2>
-                        <div className={cx('inputbox')}>
-                            <input type="email" required></input>
-                            <label for="">Email</label>
-                        </div>
-                        <div className={cx('inputbox')}>
-                            <input type="password" required></input>
-                            <label for="">Password</label>
-                        </div>
-                        <div className={cx('inputbox')}>
-                            <label for="">
-                                <input type="checkbox">
-                                    Remember Me <a href="/register">Forget Password</a>
-                                </input>
-                            </label>
-                        </div>
-                        <button>Log in</button>
-                        <div className="register">
-                            <p>
-                                Don't have a account <a href="/register">Register</a>
-                            </p>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </section>
-    );
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </div>
+      <button type="submit">Log in</button>
+    </form>
+  );
+};
 
 export default Login;
