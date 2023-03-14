3dbdd3df263c8089f4afa29160ee7dd68e09bc12
Huy <97140326+cut113@users.noreply.github.com>
2023-03-14 13:24:20 +0700

    Done FillInBlankComponent

--- src/pages/Game/fillinblank.js
+++ src/pages/Game/fillinblank.js
@@ -1,61 +1,136 @@
+// import React, { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import axios from 'axios';
+
+// function FillInTheBlankGame() {
+//     const [sentences, setSentences] = useState([]);
+//     const [answers, setAnswers] = useState([]);
+//     const { levelid } = useParams();
+
+//     useEffect(() => {
+//         // Load data from JSON file
+//         loadFillInBlank(levelid);
+//     }, [levelid]);
+
+//     const loadFillInBlank = async (levelid) => {
+//         try {
+//             const response = await axios.get(`http://localhost:8080/loadFillInBlank/${levelid}`);
+//             // console.log(response.data.map((item) => item.question));
+//             setSentences(response.data.map((item) => item.question));
+//             setAnswers(response.data.answer.map((item) => item.answer));
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     };
+
+//     function handleAnswerChange(index, value) {
+//         const newAnswers = [...answers];
+//         newAnswers[index] = value;
+//         setAnswers(newAnswers);
+//     }
+
+//     function handleSubmit(event) {
+//         event.preventDefault();
+//         // Check answers and show score
+//         const score = answers.filter(
+//             (answer, index) => answer.toLowerCase() === sentences[index].answer.toLowerCase(),
+//         ).length;
+//         alert(`You got ${score} out of ${sentences.length} correct!`);
+//     }
+
+//     if (!sentences) {
+//         return <div>Loading...</div>;
+//     }
+
+//     return (
+//         <form onSubmit={handleSubmit}>
+//            <div>
+//                 {sentences.map((sentence, index) => (
+//                     <div key={index}>
+//                         {sentence}
+//                         <input
+//                             type="text"
+//                             value={answers[index]}
+//                             onChange={(event) => handleAnswerChange(index, event.target.value)}
+//                         />
+//                         </div>
+//                 ))}        
+//            </div>
+//             <button type="submit">Submit</button>
+//         </form>
+//     );
+// }
+
+// export default FillInTheBlankGame;
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function FillInTheBlankGame() {
-    const [sentences, setSentences] = useState([]);
-    const [answers, setAnswers] = useState([]);
-    const { levelid } = useParams();
-    useEffect(() => {
-        // Load data from JSON file
-        loadFillInBlank(levelid);
-    }, []);
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
+  const [score, setScore] = useState(null);
+  const { levelid } = useParams();
 
-    const loadFillInBlank = async (levelid) => {
-        axios
-            .get(`http://localhost:8080/loadFillInBlank/${levelid}`)
-            .then((response) => {
-                console.log(response.data);
-                setSentences(response.data.question);
-                setAnswers(response.data.answer);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
+  useEffect(() => {
+    loadQuestions(levelid);
+  }, [levelid]);
 
-    function handleAnswerChange(index, value) {
-        const newAnswers = [...answers];
-        newAnswers[index] = value;
-        setAnswers(newAnswers);
+  const loadQuestions = async (levelid) => {
+    try {
+      const response = await axios.get(`http://localhost:8080/loadFillInBlank/${levelid}`);
+      setQuestions(response.data);
+      setAnswers(Array(response.data.length).fill(''));
+      setScore(null);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        // Check answers and show score
-        const score = answers.filter(
-            (answer, index) => answer.toLowerCase() === sentences[index].answer.toLowerCase(),
-        ).length;
-        alert(`You got ${score} out of ${sentences.length} correct!`);
-    }
+  const handleAnswerChange = (index, value) => {
+    const newAnswers = [...answers];
+    newAnswers[index] = value;
+    setAnswers(newAnswers);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-           <div>
-                {sentences.map((sentence, index) => (
-                    <div key={index}>
-                        {sentence}
-                        <input
-                            type="text"
-                            value={answers[index]}
-                            onChange={(event) => handleAnswerChange(index, event.target.value)}
-                        />
-                        </div>
-                ))}        
-           </div>
-            <button type="submit">Submit</button>
-        </form>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newScore = answers.filter(
+      (answer, index) => answer.toLowerCase() === questions[index].answer.toLowerCase()
+    ).length;
+    setScore(newScore);
+  };
+
+  if (!questions.length) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <h1>Fill in the Blank Game</h1>
+      <form onSubmit={handleSubmit}>
+        {questions.map((question, index) => (
+          <div key={index}>
+            <p>{question.question}</p>
+            <input
+              type="text"
+              value={answers[index]}
+              onChange={(event) => handleAnswerChange(index, event.target.value)}
+            />
+          </div>
+        ))}
+        <button type="submit">Check Answers</button>
+      </form>
+      {score !== null && (
+        <div>
+          <h2>Score: {score} out of {questions.length}</h2>
+          <button onClick={() => loadQuestions(levelid)}>Play Again</button>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default FillInTheBlankGame;
+
+
