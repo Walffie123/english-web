869a931216c9bea69a41cba3b6f45f2f9fefc598
Thanh <hondondaide@gmail.com>
2023-03-26 10:33:15 +0700

    xong mot nua Course CRUD

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,11 +4,13 @@ import Upload from '~/pages/Upload/upload';
 import Game from '~/pages/Game/game';
 import Login from '~/pages/Auth/login';
 import Register from '~/pages/Auth/register';
-import ListComponent from '~/components/ListComponent';
-import FlashCardComponent from '~/components/FlashCardComponent';
-import FlashCardLessonComponent from '~/components/FlashCardLessonComponent';
+import ListComponent from '~/components/Flashcards/ListComponent';
+import FlashCardComponent from '~/components/Flashcards/FlashCardComponent';
+import FlashCardLessonComponent from '~/components/Flashcards/FlashCardLessonComponent';
 import WordAsscociation from '../components/Games/WordAssociation/WordAss';
-import CRUDFlashCardComponent from '~/components/CRUDFlashCardComponent';
+import CRUDFlashCardComponent from '~/components/Flashcards/CRUDFlashCardComponent';
+import CourseComponent from '~/components/Course/CourseComponent';
+import CourseDetailComponent from '~/components/Course/CourseDetailComponent';
 
 //khong can Login van dung Route nay`
 const publicRoutes = [
@@ -22,7 +24,10 @@ const publicRoutes = [
     { path: '/flashcard', component: FlashCardComponent},
     { path: '/flashcard/:lessonid', component: FlashCardLessonComponent},
     { path: '/word', component: WordAsscociation },
-    { path: '/crudFlashCard/:lessonid', component: CRUDFlashCardComponent }
+    { path: '/crudFlashCard/:lessonid', component: CRUDFlashCardComponent },
+    { path: '/loadCourse', component: CourseComponent },
+    { path: '/courseDetail/:courseid', component: CourseDetailComponent }
+    
 ];
 
 //phai Login moi dung Route nay`
