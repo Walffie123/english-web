efcc5c7e9217f46ee9cf2043ed75ce7d7f744fef
Huy <97140326+cut113@users.noreply.github.com>
2023-03-30 10:11:35 +0700

    Add display PDF

--- src/components/Lesson/LessonDetailComponent.js
+++ src/components/Lesson/LessonDetailComponent.js
@@ -5,38 +5,48 @@ import classNames from 'classnames/bind';
 import styles from '../Lesson/LessonDetailComponent.module.scss';
 import { less } from 'fontawesome';
 import { Card, Button } from 'react-bootstrap';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
 
-const cx = classNames.bind(styles);
+
+
 export default function CourseDetailComponent(props) {
+    const cx = classNames.bind(styles);
+
     const [lesson, setLesson] = useState([]);
+    const [pdfPath, setPdfPath] = useState(null);
+    const [numPages, setNumPages] = useState(null);
     const { lessonid } = useParams();
     const baseUrl = 'http://localhost:8080'; // replace with your backend URL
 
+    const loadLessonByLessonId = async (lessonid) => {
+        const result = await axios.get(`${baseUrl}/findLesson/${lessonid}`);
+        setLesson(result.data);
+        setPdfPath(result.data.pdfFile);
+    };
+    
+    console.log(lesson.pdfFile);
+    // console.log(pdfPath);
     useEffect(() => {
-        loadLessonByLessonId();
+        loadLessonByLessonId(lessonid);
     }, []);
 
-    const loadLessonByLessonId = async () => {
-        const result = await axios.get(`${baseUrl}/findLesson/${lessonid}`);
-        // console.log(result.data);
-        setLesson(result.data);
-        console.log(lesson);
-    };
+   
 
     return (
         <div className="container">
-            hahahaah
             <div className="row">
-                <div className={cx('col-md-8', 'lesson-container')}>
-                    <Card className={cx('card')} style={{ width: '30rem', margin: '10px' }}>
-                        <Card.Body className="cardbody">
+                <div className={cx('col-md-8', 'lesson-container')}>   
                             <div className={cx('lesson')}>
-                                <Card.Title className={cx('cardtitle')}>Lesson: {lesson.lessonName}</Card.Title>
-                                <Card.Text className={cx('cardtext')}>{lesson.content}</Card.Text>
+                                <h1>Lesson: {lesson.lessonName}</h1>
+                               
+                                {
+                                    pdfPath && (
+                                      <a href={pdfPath} target="_blank">Click here to open PDF file</a>  
+                                    )
+                                }
+                                
+                                <p>{lesson.content}</p>
                             </div>
-                        </Card.Body>
-                    </Card>
-
                     <Button href={`/flashcard/${lessonid}`}>Flash Card</Button>
                 </div>
             </div>
